7b2ff37ab38a679c9cdc85e7877a23e1d7e9cb5e
keisukemukaida <keisukemukaida>
2022-04-08 14:07:10 +0900

    汎用処理　商品検索 ダミーデータを返すように修正

--- code-reader-master/app/src/main/java/net/mm2d/codereader/util/Product.kt
+++ code-reader-master/app/src/main/java/net/mm2d/codereader/util/Product.kt
@@ -1,7 +1,11 @@
 package net.mm2d.codereader.util
 
-object Product {
-    fun productSearch(jan: String) {
+import net.mm2d.codereader.model.Product
+import net.mm2d.codereader.model.ProductVariation
 
+object Product {
+    fun productSearch(jan: String): ProductVariation {
+        val dummyProduct = Product("1", "BLOOD", "ブラッドハウンドTシャツ")
+        return ProductVariation("BLOOD001", dummyProduct, "Sサイズ", "赤")
     }
 }
