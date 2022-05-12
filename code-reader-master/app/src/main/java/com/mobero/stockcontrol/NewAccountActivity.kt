b57ae1abce0dce2ea048971d037a02e6eb11b159
koudait <koudai.tajima@mobero-corp.com>
2022-05-12 10:14:22 +0900

    アカウント作成機能実装。

--- code-reader-master/app/src/main/java/com/mobero/stockcontrol/NewAccountActivity.kt
+++ code-reader-master/app/src/main/java/com/mobero/stockcontrol/NewAccountActivity.kt
@@ -1,14 +1,18 @@
 package com.mobero.stockcontrol
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.GenericHandler
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.cognitoidentityprovider.model.SignUpResult
@@ -32,11 +36,33 @@ class NewAccountActivity : AppCompatActivity() {
             signUp(textEmail.text.toString(), textPassword.text.toString(), {
                 Toast.makeText(this, "Success!", Toast.LENGTH_LONG).show()
                 // 確認コード入力ダイアログを表示する
-                val confirmFlg = true
-                if (confirmFlg) {
-                    // 認証が確認取れたら認証APIを叩く
-                    // 認証APIが成功したら自動でサインインしてメインメニューに遷移
-                }
+
+                val editText = AppCompatEditText(this)
+                AlertDialog.Builder(this)
+                    .setTitle("確認コード")
+                    .setMessage("確認コードを入力してください。")
+                    .setView(editText)
+                    .setPositiveButton("OK") { dialog, _ ->
+                        // OKボタンを押したときの処理
+                        val code = editText.text.toString()
+                        //確認コード判定API呼び出し
+                        confirmSignUp(textEmail.text.toString(),code,{
+                            // 確認コードが正しいとき、ダイアログを閉じて、サインインしてメインメニューに遷移
+                            dialog.dismiss()
+                            val intent = Intent(this, MenuActivity::class.java)
+                            startActivity(intent)
+                        },{
+                            // 確認コードが正しくないとき、エラーメッセージを表示
+                            Toast.makeText(this, "failed!" + it?.message, Toast.LENGTH_LONG).show()
+                        })
+                    }
+                    .setNegativeButton("キャンセル") { dialog, _ ->
+                        // キャンセルボタンを押したとき、ダイアログを閉じる
+                        dialog.dismiss()
+                    }
+                    .create()
+                    .show()
+
             }, {
                 Toast.makeText(this, "failed!" + it?.message, Toast.LENGTH_LONG).show()
             })
@@ -63,5 +89,25 @@ class NewAccountActivity : AppCompatActivity() {
             }
         )
     }
+
+    //本人確認
+    private fun confirmSignUp(email: String,
+                      verifyCode: String,
+                      successCallback: () -> Unit,
+                      failureCallback: (exception: Exception?) -> Unit
+    ) {
+        userPool.getUser(email).confirmSignUpInBackground(verifyCode, false,
+            object : GenericHandler {
+                override fun onSuccess() {
+                    successCallback()
+                }
+
+                override fun onFailure(exception: Exception?) {
+                    failureCallback(exception)
+                }
+            })
+    }
+
+
 
 }
