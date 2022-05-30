efc3431fcccc8e0b46cf87a3a46464e6c066ce35
koudait <koudai.tajima@mobero-corp.com>
2022-05-30 12:57:37 +0900

    出納検索画面作成_不足分コミット

--- code-reader-master/app/src/main/java/com/mobero/stockcontrol/model/Stock.kt
+++ code-reader-master/app/src/main/java/com/mobero/stockcontrol/model/Stock.kt
@@ -1,4 +1,3 @@
 package com.mobero.stockcontrol.model
 
-open class Stock(clientCode: String, productCode: String, productName: String, prvId: Int, uniqueCode: String, sizeName: String, colorName: String, var loc: Location?, var stockNum: Int): ProductVariation(clientCode, productCode, productName, prvId, uniqueCode, sizeName, colorName) {
-}
+open class Stock(clientCode: String, productCode: String, productName: String, prvId: Int, uniqueCode: String, sizeName: String, colorName: String, var loc: Location?, var stockNum: Int): ProductVariation(clientCode, productCode, productName, prvId, uniqueCode, sizeName, colorName)
